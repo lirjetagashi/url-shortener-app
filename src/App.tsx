c6465c76bb6009413a9a9491a80e94f94ff40e5b
Lirjeta Gashi <lirjeta.gashi1@gmail.com>
2023-10-25 23:47:26 +0200

    initial setup/added ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Flex, Heading, Image, Text, Input, Button} from "@chakra-ui/react";
 
 function App() {
+  // const [shortenedUrls, setShortenedUrls] = useState<string[]>([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Flex height={'100vh'}>
+            <Flex width={'20%'} height={'100%'} backgroundColor={'#EFEFEF'} alignItems={'center'} flexDirection={'column'}>
+                <Image src={'/AnchorzUp-logo.svg'} width={'150px'} height={'150px'} />
+                <Text fontWeight={'bold'} fontSize={'lg'}>My shortened URLs</Text>
+            </Flex>
+            <Flex flexDirection={'column'} alignItems={'flex-start'} p={'5em 7em'} gap={10}>
+                <Heading>URL Shortener</Heading>
+                <Flex flexDirection={'column'} gap={8}>
+                    <Input placeholder={'Paste the URL to be shortened'} width={'md'} height={'50px'} border={'1px solid black'} borderRadius={0} fontSize={'20px'} />
+                    <Button width={'150px'} height={'50px'} borderRadius={0} bg={'#92278F'} color={'white'}>Shorten URL</Button>
+                </Flex>
+            </Flex>
+        </Flex>
+    </>
   );
 }
 
